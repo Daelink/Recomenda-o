4fa442ad99c6999056fc6db94651359f80b7c2e7
Daelink <daelinkfirebase@gmail.com>
2024-10-03 09:47:03 -0300

    Update App.py

--- App.py
+++ App.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configurar o Firebase
-cred_path = os.path.join(os.path.dirname(__file__), 'Database/FirebaseDaeLink.json')
+cred_path = os.path.join(os.path.dirname(__file__), 'FirebaseDaeLink.json')
 
 if not os.path.exists(cred_path):
     print("Arquivo de credenciais não encontrado. Verifique o caminho.")
